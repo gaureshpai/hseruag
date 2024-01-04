20ced89113e0203591ea3cdfb6b1aef3ada603d4
Gauresh G Pai <paigauresh@gmail.com>
2024-01-04 19:46:09 +0530

    code upload

--- procucerconsumer.c
+++ procucerconsumer.c
@@ -9,3 +9,33 @@ void producer(){
     printf("\nProducer produces item %d",x);
     ++mutex;
 }
+void consumer(){
+    --mutex;
+    --full;
+    ++empty;
+    printf("\nConsumer consumes item %d",x);
+    x--;
+    ++mutex;
+}
+int main(){
+    int n,i;
+    print("\nPress 1 for producer\n2.Press 2 for consumer\n3.Press 3 for exit");
+    for(i=;i>0;i++){
+        printf("\nEnter your choice:");
+        scanf("%d",&n);
+        switch(n){
+            case 1:if((mutex==1)&&(empty!=0))
+                        producer();
+                    else
+                        printf("Buffer is full!");
+                    break;
+            case 2:if ((mutex == 1) && (full != 0))
+                        consumer();
+                    else
+                        printf("Buffer is empty!");
+                    break;
+            case 3:exit(0);
+                    break;
+        }
+    }
+}
