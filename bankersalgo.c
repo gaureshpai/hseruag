9e30cb3cb6e28736fbea23db693cda95cbd93269
Gauresh G Pai <paigauresh@gmail.com>
2024-01-05 17:07:47 +0530

    done

--- bankersalgo.c
+++ bankersalgo.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
+
 int main(){
     int p,c,count=0,i,j,alc[5][3],max[5][3],need[5][3],safe[5],available[3],done[5],terminate=0;
-    printf("ENter the number of processes and resources");
+
+    printf("Enter the number of processes and resources");
     scanf("%d%d",&p,&c);
+
     printf("Enter allocation of resource of all process %dx%d matrix",p,c);
     for(i=0;i<p;i++)
     for(j=0;j<c;j++)
     scanf("%d",&max[i][j]);
+
     printf("Enter the available resources");
     for(i=0;i<c;i++){
         scanf("%d",&available[i]);
     }
+
     printf("\nNeed resources matrix are\n");
     for(i=0;i<p;i++){
         for(j=0;j<c;j++){
@@ -19,6 +24,7 @@ int main(){
         }
         printf("\n");
     }
+
     for(i=o;i<p;i++)
         done[i] = 0;
         while(count<p){
@@ -28,6 +34,7 @@ int main(){
                     if(need[i][j]>available[j])
                         break;
                 }
+
                 if(j==c){
                     safe[count] = i;
                     done[i] = 1;
@@ -36,6 +43,7 @@ int main(){
                     count++;
                     terminate=0;
                 }
+
                 else{
                     terminate++;
 
@@ -51,6 +59,7 @@ int main(){
             for(i=0;i<c;i++)
                 printf("%d\t",available[i]);
             printf("\nSafe sequence are\n");
+            
             for(i=0;i<n;i++)
                 printf("p %d\t",safe[i]);
         }
