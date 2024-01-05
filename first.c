f88f5865b585465576d500a2e3e4c3a023ec5b77
Gauresh G Pai <paigauresh@gmail.com>
2024-01-05 17:05:42 +0530

    fisrt

--- first.c
+++ first.c
@@ -2,12 +2,16 @@
 
 int main(){
     char name[25],college[25],adddress[25];
+
     printf("Enter your name:");
     scanf("%s",name);
+
     printf("Enter your college name:");
     scanf("%s",college);
+
     printf("Enter your address:");
     scanf("%s",adddress);
+    
     printf("Name = %s",name);
     printf("College = %s",college);
     printf("Address = %s",adddress);
