a43e2d0a82e4af56a5e7dd6a36b5a0f29deccdd2
gaureshpai <paigauresh@gmail.com>
2026-07-31 00:13:03 +0530

    fix: skip MainLayout for index route so iframe page renders without navbar/footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -84,12 +84,16 @@ export default function App({ Component, pageProps }: AppProps) {
         ]}
       />
       <ThemeProvider attribute="class" defaultTheme="light">
-        <MainLayout>
-          <AnimatePresence mode="wait" initial={false}>
-            <CursorTrailCanvas className="pointer-events-none fixed inset-0 -z-10 hidden h-full w-full md:flex" />
-            <Component key={router.asPath} {...pageProps} />
-          </AnimatePresence>
-        </MainLayout>
+        {router.pathname === "/" ? (
+          <Component key={router.asPath} {...pageProps} />
+        ) : (
+          <MainLayout>
+            <AnimatePresence mode="wait" initial={false}>
+              <CursorTrailCanvas className="pointer-events-none fixed inset-0 -z-10 hidden h-full w-full md:flex" />
+              <Component key={router.asPath} {...pageProps} />
+            </AnimatePresence>
+          </MainLayout>
+        )}
       </ThemeProvider>
     </>
   );
